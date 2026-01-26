b70afa3086e79baf5ab9658e9614bf227fbbaba0
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-01-27 00:19:14 +0530

    Initial commit
    Setup project structure

---
+++ AutoMap-Project-Back-end/src/main/java/com/AutoMap/AutoMap_Project_Back_end/AutoMapProjectBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.AutoMap.AutoMap_Project_Back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoMapProjectBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoMapProjectBackEndApplication.class, args);
+	}
+
+}
---
+++ AutoMap-Project-Back-end/src/test/java/com/AutoMap/AutoMap_Project_Back_end/AutoMapProjectBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AutoMap.AutoMap_Project_Back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoMapProjectBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
